dd5d013d6c354fb1467b5887c7abc978ece71eec
yusufokaraman <yusufokaraman@gmail.com>
2023-07-16 23:48:56 +0300

    Task( async- await ) usage.

--- TaskFormApp/Form1.cs
+++ TaskFormApp/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -21,11 +15,17 @@ namespace TaskFormApp
             InitializeComponent();
         }
 
+        /// <summary>
+        /// File Read Button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void button1_Click(object sender, EventArgs e)
         {
             string data = string.Empty;
-                
-            Task<string> read = ReadFileAsync();
+
+            //Task<string> read = ReadFileAsync();
+            Task<string> read = ReadFileAsyncSecond();
             richTextBox2.Text = await new HttpClient().GetStringAsync("https://fenerbahce.org");
 
             data = await read;
@@ -33,21 +33,35 @@ namespace TaskFormApp
             richTextBox1.Text = data;
         }
 
+        /// <summary>
+        /// Counter button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click_1(object sender, EventArgs e)
         {
             textBoxCounter.Text = counter++.ToString();
         }
+
+        /// <summary>
+        /// Sync File Read.
+        /// </summary>
+        /// <returns>data.</returns>
         private string ReadFile()
         {
             string data = string.Empty;
             using (StreamReader s = new StreamReader("dosya.txt"))
-                    {
-                         Thread.Sleep(5000);
-                         data = s.ReadToEnd();          
-                    }
+            {
+                Thread.Sleep(5000);
+                data = s.ReadToEnd();
+            }
             return data;
         }
-        
+
+        /// <summary>
+        /// Async File Read.
+        /// </summary>
+        /// <returns> data.</returns>
         private async Task<string> ReadFileAsync()
         {
             var data = string.Empty;
@@ -55,11 +69,27 @@ namespace TaskFormApp
             using (StreamReader sr = new StreamReader("dosya.txt"))
             {
                 Task<string> myTask = sr.ReadToEndAsync();
+                /// async kullanım senaryosu
+                /// Asny method çağrıldığında burada yapacağın ektra işler var ise, async await kullan ki aradaki işlemleri yap.
+                /// Eğer ki StreamReader ı çalıştırırken yapacağın ekstra işler yoksa direkt geri dönebilirsin.
 
                 await Task.Delay(5000);
                 data = await myTask;
                 return data;
             }
+        }
+
+        /// <summary>
+        /// Direct return Task.
+        /// When Task returns directly, async-await are unnecesary.
+        /// </summary>
+        /// <returns>Task.</returns>
+        private Task<string> ReadFileAsyncSecond()
+        {
+            StreamReader sr = new StreamReader("dosya.txt");
+
+            return sr.ReadToEndAsync();
+
         }
     }
 }
---
+++ TaskWebApp.API/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TaskWebApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetContentAsync()
+        {
+            var myTask = new HttpClient().GetStringAsync("https://fenerbahce.org");
+
+            var data = await myTask;
+
+            return Ok(data);
+        }
+    }
+}
